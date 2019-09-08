024a94c73bab7ea32d5f0c4cb750c23d70004a5e
idlesolution <idlesoluszyn@gmail.com>
2019-09-08 16:05:08 +0200

    Added new command

---
+++ Modules/PurgeCommand.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+
+namespace NotifyBot.Modules
+{
+    public class PurgeCommand : ModuleBase<SocketCommandContext>
+    {
+        [Command("purge")]
+        [Name("purge <amount>")]
+        [Summary("Deletes a specified amount of messages")]
+        [RequireBotPermission(GuildPermission.ManageMessages)]
+        [RequireUserPermission(GuildPermission.ManageMessages)]
+        public async Task DelMesAsync(int delnum)
+        {
+            var messages = await Context.Channel.GetMessagesAsync(delnum).FlattenAsync();
+            await ((ITextChannel) Context.Channel).DeleteMessagesAsync(messages);
+        } 
+    }
+}
